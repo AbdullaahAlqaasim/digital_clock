492963f6c81525597ce41ec421500f0c527612ca
ABDULLAAH ALQAASIM <abdullahalqaasim.uae@gmail.com>
2022-05-04 15:42:55 +0400

    Update digitalclock.py

--- digital clock/digitalclock.py
+++ digital clock/digitalclock.py
@@ -7,8 +7,6 @@ digital clock implementation by ABDULLAAH ALQAASIM
 Linked in  @abdullaah alqaasim :  https://www.linkedin.com/in/abdullaah-alqaasim-30b960209/
 Instagram @abdullaah_alqaasim: https://www.instagram.com/abdullaah_alqaasim/ 
 Github: https://github.com/AbdullaahAlqaasim 
-
-youtube : 
 """
 
 
